0ff95f8132c9f643cf79b53b06fc8bb7496c88ac
priyanshu150106 <priyanshu150106@gmail.com>
2026-03-24 15:03:23 +0530

    UC2: Renamed class to BookMyStayApp and implemented static room availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,23 +1,27 @@
-/**
- * Hotel Booking Management System
- * Entry Point of Application
- *
- * This class demonstrates the starting point of the application.
- *
- * @author YourName
- * @version 1.0
- */
+import java.util.ArrayList;
+
 public class BookMyStayApp {
 
-    /**
-     * Main method - JVM starts execution here
-     */
     public static void main(String[] args) {
 
         System.out.println("=================================");
         System.out.println(" Welcome to Hotel Booking System ");
-        System.out.println(" Version: 1.0 ");
-        System.out.println("=================================");
+        System.out.println(" Version: 1.1 ");
+        System.out.println("=================================\n");
 
+        ArrayList<String> roomTypes = new ArrayList<>();
+        roomTypes.add("Single Room");
+        roomTypes.add("Double Room");
+        roomTypes.add("Suite");
+
+        int[] availability = {5, 3, 2};
+
+        System.out.println("Available Room Types:\n");
+
+        for (int i = 0; i < roomTypes.size(); i++) {
+            System.out.println("Room Type: " + roomTypes.get(i));
+            System.out.println("Available Rooms: " + availability[i]);
+            System.out.println("-----------------------------");
+        }
     }
 }
